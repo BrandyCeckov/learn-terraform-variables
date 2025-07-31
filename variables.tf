f214193fa89c69672c69d847b0d44eb36fb8b686
Jozef Kucerik <jozef.kucerik@eset.com>
2025-07-31 22:47:17 +0200

    Refactor AWS provider configuration to use variables for region and VPC CIDR block

--- variables.tf
+++ variables.tf
@@ -2,3 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # Variable declarations
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
