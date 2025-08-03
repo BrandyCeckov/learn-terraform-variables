235f8f483a73f7500c8eab2c7a42ff26d59d9300
Jozef Kucerik <jozef.kucerik@eset.com>
2025-08-03 13:21:39 +0200

    Update security group names to use dynamic tags and add validation for resource tags

--- variables.tf
+++ variables.tf
@@ -69,10 +69,25 @@ variable "private_subnet_cidr_blocks" {
 }
 
 variable "resource_tags" {
-  description = "Tags to set for all resource"
-  type = map(string)
-  default = {
-    project     = "project-alpha",
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default     = {
+    project     = "my-project",
     environment = "dev"
   }
+
+  validation {
+    condition     = length(var.resource_tags["project"]) <= 16 && length(regexall("[^a-zA-Z0-9-]", var.resource_tags["project"])) == 0
+    error_message = "The project tag must be no more than 16 characters, and only contain letters, numbers, and hyphens."
+  }
+
+  validation {
+    condition     = length(var.resource_tags["environment"]) <= 8 && length(regexall("[^a-zA-Z0-9-]", var.resource_tags["environment"])) == 0
+    error_message = "The environment tag must be no more than 8 characters, and only contain letters, numbers, and hyphens."
+  }
+}
+
+variable "ec2_instance_type" {
+  description = "aws ec2 instance type"
+  type = string
 }
